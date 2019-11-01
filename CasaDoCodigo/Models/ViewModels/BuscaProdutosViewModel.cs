bc6c7310e313878d6b901ea08503a08b7204c95c
Felipe Balascio <fbalascio@luxfacta.com>
2019-11-01 15:37:00 -0300

    adicionando viewmodel

---
+++ CasaDoCodigo/Models/ViewModels/BuscaProdutosViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models.ViewModels
+{
+    public class BuscaProdutosViewModel
+    {
+        public IList<Categoria> Categorias { get; set; }
+        public string Pesquisa { get; set; }
+        public string PesquisaErro { get; set; }
+
+        public BuscaProdutosViewModel(IList<Categoria> categorias, string pesquisa, string erro)
+        {
+            Categorias = categorias;
+            Pesquisa = pesquisa;
+            PesquisaErro = erro;
+        }
+        public BuscaProdutosViewModel()
+        {
+
+        }
+            
+    }
+}
